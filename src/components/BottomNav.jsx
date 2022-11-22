3083a51ee47df5c361c4b03cb8226812dba71d6f
pedrocruzio <pedro@rokailabs.com>
2022-11-22 09:31:52 -0400

    update colors

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -6,6 +6,8 @@ import videos from '../assets/videos.png';
 import live from '../assets/live.png';
 import tv from '../assets/tv.png';
 import perfil from '../assets/perfil.png'
+import { Typography } from "@mui/material";
+
 
 
 function BottomNav() {
@@ -15,23 +17,43 @@ function BottomNav() {
         <div className={styles.mainNav}>
             <div className={styles.home} >
                 <img src={home} alt='home' style={{opacity: 0.2}}/>
-                <div className={styles.navbarText}>Noticias</div>
+                <div className={styles.navbarText}>
+                    <Typography variant="caption" mb={2} sx={{ color: "white", opacity: 0.2 }}>
+                        Noticias
+                    </Typography>
+                </div>
             </div>
             <div className={styles.search} >
                 <img src={videos} alt='home'/>
-                <div className={styles.navbarText}>Videos</div>
+                <div className={styles.navbarText}>
+                <Typography variant="caption" mb={2} sx={{ color: "white" }}>
+                    Videos
+                </Typography>
+                </div>
             </div>
             <div className={styles.newpost} >
                     <img src={tv} alt='newpost' style={{opacity: 0.2}}/>
-                    <div className={styles.navbarText}>TV</div>
+                    <div className={styles.navbarText}>
+                    <Typography variant="caption" mb={2} sx={{ color: "white", opacity: 0.2 }}>
+                        TV
+                    </Typography>
+                    </div>
                 </div>
             <div className={styles.heart} >
                 <img src={live} alt='radio' style={{opacity: 0.2}}/>
-                <div className={styles.navbarText}>Radio</div>
+                <div className={styles.navbarText}>
+                <Typography variant="caption" mb={2} sx={{ color: "white", opacity: 0.2 }}>
+                    Radio
+                </Typography>
+                </div>
             </div>
             <div className={styles.heart} >
                 <img src={perfil} alt='perfil' style={{opacity: 0.2}}/>
-                <div className={styles.navbarText}>Perfil</div>
+                <div className={styles.navbarText}>
+                <Typography variant="caption" mb={2} sx={{ color: "white", opacity: 0.2 }}>
+                    Perfil
+                </Typography>
+                </div>
             </div>
         </div>
     )
